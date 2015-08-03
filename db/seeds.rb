2cd0d586703278c136d89f0c25599bf21369887b
simm19 <simm19@hotmail.fr>
2015-08-03 17:01:07 +0000

    add admin seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+user = User.new(
+      :email                 => "simon@bitit.gift",
+      :password              => "ouvretoi",
+      :password_confirmation => "ouvretoi"
+  )
+  user.save!
